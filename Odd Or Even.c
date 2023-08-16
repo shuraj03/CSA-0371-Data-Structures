a003b9516ea4b98616478e372113e553cb2b52b7
SUNIL SHURAJ N <118829941+shuraj03@users.noreply.github.com>
2023-08-16 09:55:34 +0530

    Add files via upload

---
+++ Odd Or Even.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+  int n;
+  printf("Enter the Integer\n");
+  scanf("%d",&n);
+  if (n%2==0){
+    printf("%d Is a Even Number", n);
+  }
+  else{
+    printf("%d Is a Odd Number",n);
+  }
+return 0;
+}
+  
